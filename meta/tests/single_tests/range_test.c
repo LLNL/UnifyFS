f937dd5e14903257a81b61f0d865a8be264f9012
dsikich <sikich1@llnl.gov>
2017-12-08 15:39:30 -0800

    fix burstfs and cruise license (#15)
    Updated license text to say burstfs or cruise instead of unifycr. When the
    function and variable names were updated to say unifycr instead of burstfs, the
    license text for the burstfs text also got changed to unifycr. These updates
    correct the license text to use the burstfs copyright. Also, the cruise
    copyright was also updated similarly because it had the same issue. All full
    license text was added at the top level of the repository.

--- meta/tests/single_tests/range_test.c
+++ meta/tests/single_tests/range_test.c
@@ -4,31 +4,18 @@
  * Copyright (c) 2017, Florida State University. Contributions from
  * the Computer Architecture and Systems Research Laboratory (CASTL)
  * at the Department of Computer Science.
- * Written by
- * 	Teng Wang tw15g@my.fsu.edu
- * 	Adam Moody moody20@llnl.gov
- * 	Weikuan Yu wyu3@fsu.edu
- * 	Kento Sato kento@llnl.gov
- * 	Kathryn Mohror. kathryn@llnl.gov
- * 	LLNL-CODE-728877.
- * All rights reserved.
  *
- * This file is part of UnifyCR For details, see https://github.com/llnl/unifycr.
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights to
- * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
- * the Software, and to permit persons to whom the Software is furnished to do so,
- * subject to the following conditions:
- * The above copyright notice and this permission notice shall be included in all
- * copies or substantial portions of the Software.
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
- * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
- * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ * Written by: Teng Wang, Adam Moody, Weikuan Yu, Kento Sato, Kathryn Mohror
+ * LLNL-CODE-728877. All rights reserved.
  *
+ * This file is part of burstfs. For details, see https://github.com/llnl/burstfs
+ * Please read https://github.com/llnl/burstfs/LICENSE for full license text.
+ */
+
+/*
+ *
+ * Copyright (c) 2014, Los Alamos National Laboratory
+ *	All rights reserved.
  *
  */
 
--- server/src/unifycr_service_manager.c
+++ server/src/unifycr_service_manager.c
@@ -1,16 +1,16 @@
 /*
-* Copyright (c) 2017, Lawrence Livermore National Security, LLC.
-* Produced at the Lawrence Livermore National Laboratory.
-* Copyright (c) 2017, Florida State University. Contributions from
-* the Computer Architecture and Systems Research Laboratory (CASTL)
-* at the Department of Computer Science.
-*
-* Written by: Teng Wang, Adam Moody, Weikuan Yu, Kento Sato, Kathryn Mohror
-* LLNL-CODE-728877. All rights reserved.
-*
-* This file is part of UnifyCR. For details, see https://github.com/llnl/unifycr
-* Please read https://github.com/llnl/unifycr/LICENSE for full license text.
-*/
+ * Copyright (c) 2017, Lawrence Livermore National Security, LLC.
+ * Produced at the Lawrence Livermore National Laboratory.
+ * Copyright (c) 2017, Florida State University. Contributions from
+ * the Computer Architecture and Systems Research Laboratory (CASTL)
+ * at the Department of Computer Science.
+ *
+ * Written by: Teng Wang, Adam Moody, Weikuan Yu, Kento Sato, Kathryn Mohror
+ * LLNL-CODE-728877. All rights reserved.
+ *
+ * This file is part of burstfs. For details, see https://github.com/llnl/burstfs
+ * Please read https://github.com/llnl/burstfs/LICENSE for full license text.
+ */
 
 #include <stdlib.h>
 #include <unistd.h>
