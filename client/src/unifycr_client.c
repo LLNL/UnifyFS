e2b4e6371547ea31c9a3eba6377a9e4ae5c8b3dc
jgmoore-or <jgmoore.ornl@gmail.com>
2018-06-20 12:36:19 -0400

    fixed fix of merge problem

--- client/src/unifycr_client.c
+++ client/src/unifycr_client.c
@@ -11,47 +11,8 @@
 #include "unifycr_client.h"
 #include "unifycr_clientcalls_rpc.h"
 
+/* declaration should be moved to header file */
 int unifycr_sync_to_del(unifycr_mount_in_t* in);
-#if 0
-{
-    int num_procs_per_node = local_rank_cnt;
-    int req_buf_sz = shm_req_size;
-    int recv_buf_sz = shm_recv_size;
-    long superblock_sz = glb_superblock_size;
-    long meta_offset = (void *)unifycr_indices.ptr_num_entries -
-        unifycr_superblock;
-    long meta_size = unifycr_max_index_entries * sizeof(unifycr_index_t);
-    long fmeta_offset = (void *)unifycr_fattrs.ptr_num_entries -
-        unifycr_superblock;
-    long fmeta_size = unifycr_max_fattr_entries * sizeof(unifycr_fattr_t);
-    long data_offset = (void *)unifycr_chunks - unifycr_superblock;
-    long data_size = (long)unifycr_max_chunks * unifycr_chunk_size;
-    char external_spill_dir[UNIFYCR_MAX_FILENAME] = {0};
-
-    strcpy(external_spill_dir, external_data_dir);
-
-/*
- *
- * Copy the client-side information to the
- * input struct
-*/
-    in->app_id             = app_id;
-    in->local_rank_idx     = local_rank_idx;
-    in->dbg_rank           = dbg_rank;
-    in->num_procs_per_node = num_procs_per_node;
-    in->req_buf_sz         = req_buf_sz;
-    in->recv_buf_sz        = recv_buf_sz;
-    in->superblock_sz      = superblock_sz;
-    in->meta_offset        = meta_offset;
-    in->meta_size          = meta_size;
-    in->fmeta_offset       = fmeta_offset;
-    in->fmeta_size         = fmeta_size;
-    in->data_offset        = data_offset;
-    in->data_size          = data_size;
-    in->external_spill_dir = external_spill_dir;
-    return 0;
-}
-#endif
 
 static int set_global_file_meta(unifycr_metaset_in_t* in,
                                 unifycr_fattr_t* f_meta)
@@ -88,7 +49,6 @@ uint32_t unifycr_client_mount_rpc_invoke(unifycr_client_rpc_context_t** unifycr_
 
     /* fill in input struct by calling unifycr_sync_to_del */
     unifycr_sync_to_del(&in);
-	printf("rpc external_spill_dir: [%s]\n", in.external_spill_dir);
     hret = margo_forward(handle, &in);
     assert(hret == HG_SUCCESS);
 
@@ -96,7 +56,7 @@ uint32_t unifycr_client_mount_rpc_invoke(unifycr_client_rpc_context_t** unifycr_
     hret = margo_get_output(handle, &out);
     assert(hret == HG_SUCCESS);
 
-    printf("Got response ret: %d\n", out.ret);
+	unifycr_key_slice_range = out.max_recs_per_slice;
 
     margo_free_output(handle, &out);
     margo_destroy(handle);
