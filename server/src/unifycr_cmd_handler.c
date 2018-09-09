b7fbb5332586be693952f6095ade2e38bfdea88d
Hyogi Sim <hyogi@vt.edu>
2018-09-09 11:52:12 -0400

    The previous commit (#768dc09) calculates a wrong address for handling COMM_META_GET, which leads to a failure when looking for an existing file. This fixes it.

--- server/src/unifycr_cmd_handler.c
+++ server/src/unifycr_cmd_handler.c
@@ -83,7 +83,7 @@ int delegator_handle_command(char *ptr_cmd, int sock_id)
         /*get file attribute*/
         unifycr_file_attr_t attr_val;
 
-        fattr_key_t _fattr_key = *((int *)(ptr_cmd + 2 * sizeof(int)));
+        fattr_key_t _fattr_key = *((int *)(ptr_cmd + 1 * sizeof(int)));
 
         rc = meta_process_attr_get(&_fattr_key, &attr_val);
 
