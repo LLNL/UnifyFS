9dfc7c735f444e0a1d112a5d2140b855962d337f
Adam Moody <moody20@llnl.gov>
2018-11-12 13:27:58 -0800

    simplify and comment server debug stream

--- server/src/unifycr_debug.c
+++ server/src/unifycr_debug.c
@@ -31,30 +31,36 @@
 #include "unifycr_debug.h"
 #include "unifycr_const.h"
 
+/* pointer to debug file stream */
 FILE *dbg_stream = NULL;
-char dbg_line[GEN_STR_LEN] = {0};
 
+/* open specified file as debug file stream,
+ * returns UNIFYCR_SUCCESS on success */
 int dbg_open(char *fname)
 {
     dbg_stream = fopen(fname, "a");
     if (dbg_stream == NULL) {
+        /* failed to open file name, fall back to stderr */
         dbg_stream = stderr;
         return (int)UNIFYCR_ERROR_DBG;
     } else {
-        return ULFS_SUCCESS;
+        return UNIFYCR_SUCCESS;
     }
-
 }
 
+/* close our debug file stream,
+ * returns UNIFYCR_SUCCESS on success */
 int dbg_close()
 {
     if (dbg_stream == NULL) {
+        /* nothing to close */
         return (int)UNIFYCR_ERROR_DBG;
     } else {
-        if (fclose(dbg_stream) == 0) {
-            return ULFS_SUCCESS;
+        /* if stream is open, and its not stderr, close it */
+        if (dbg_stream != stderr &&
+            fclose(dbg_stream) == 0) {
+            return UNIFYCR_SUCCESS;
         }
         return (int)UNIFYCR_ERROR_DBG;
-
     }
 }
