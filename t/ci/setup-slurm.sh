36a023f72161a2daab48bdb803f67d36aabad342
CamStan <stanavige1@llnl.gov>
2021-01-21 17:20:17 -0800

    Implement producer-consumer CI tests
    Add ability to run producer-consumer CI tests when using an allocation
    of two or more nodes. The write example is used on the initial half of
    the nodes within an allocation as the producer, and the read example
    is used on the latter half of the nodes with an allocation as the
    consumer. Currently not set up to account for an odd number of nodes
    within an allocation.
    
    Suite runs the same series of tests as the other suites; using varying
    sizes, shared file or file-per-process, and the static or gotcha
    linked executables. It defaults to using POSIX-IO. Running with MPI-IO
    is also available, however they are not being run in RUN_CI_TESTS.sh
    as there currently are bugs when running the write/read examples with
    MPI-IO.
    
    Each test's parameters are passed to `unify_run_test` using producer or
    consumer in the app name. This is to prevent file name conflicts
    within UnifyFS, or in error and output file names. The
    `build_test_command` function has been adjusted to parse this name out
    and in turn run the appropriate example executable.
    Within these functions other adjustments have been made to account for
    the producer-consumer workflow. These include using the correct
    filename when consuming and using a JOB_RUN_COMMAND that excludes half
    of the available hosts.
    As future workflows are added, may want to refactor these functions
    into separate workflow functions.
    
    Functions to get the latter and initial hosts as a comma separated
    list as well as variables needed for running on half the allocated
    nodes are set up in the respective resource manager setup scripts.

--- t/ci/setup-slurm.sh
+++ t/ci/setup-slurm.sh
@@ -19,14 +19,47 @@ get_hostlist()
 
 # Variables specific to SLURM
 nnodes=$SLURM_NNODES
+
 nres_sets=$SLURM_NNODES
 nprocs=${UNIFYFS_CI_NPROCS:-1}
 
 app_out="-o"
 app_err="-e"
-JOB_RUN_COMMAND="srun -N${nnodes} --ntasks-per-node=${nprocs}"
-JOB_RUN_ONCE_PER_NODE="srun -n${nnodes}"
+JOB_RUN_COMMAND="srun -N ${nnodes} --ntasks-per-node=${nprocs}"
+JOB_RUN_ONCE_PER_NODE="srun -n ${nnodes}"
 JOB_ID=${JOB_ID:-$SLURM_JOBID}
+
+# Set up producer-consumer variables and functions when using two or more hosts
+if [[ $nnodes -ge 2 ]]; then
+    # Prereq for running 300-producer-consumer-tests.sh
+    test_set_prereq MULTIPLE_HOSTS
+
+    # Get list of unique host names
+    # Only want to run once as order can change each time
+    # One alternative is to parse SLURM_NODELIST, which can get ugly
+    SLURM_HOSTS=$($JOB_RUN_ONCE_PER_NODE hostname)
+
+    # Get the initial half of the hosts in the current allocation
+    get_initial_hosts()
+    {
+        local l_initial_hosts=$(echo "$SLURM_HOSTS" | head -n $(($nnodes / 2)))
+        # Replace spaces with commas
+        local l_initial_hostlist=$(echo $l_initial_hosts | tr ' ' ',')
+        echo $l_initial_hostlist
+    }
+
+    # Get the latter half of the hosts in the current allocation
+    get_latter_hosts()
+    {
+        local l_latter_hosts=$(echo "$SLURM_HOSTS" | tail -n $(($nnodes / 2)))
+        # Replace spaces with commas
+        local l_latter_hostlist=$(echo $l_latter_hosts | tr ' ' ',')
+        echo $l_latter_hostlist
+    }
+
+    exclude_option="-x"
+    JOB_RUN_HALF_NODES="srun -N $(($nnodes/2)) --ntasks-per-node=${nprocs}"
+fi
 
 echo "$infomsg ====================== SLURM Job Info ======================"
 echo "$infomsg ------------------------ Job Status ------------------------"
