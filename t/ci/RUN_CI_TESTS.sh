278cac59a17d6aa9fec52fdb59aafbefb57e5167
CamStan <stanavige1@llnl.gov>
2021-01-21 17:20:17 -0800

    Implement producer-consumer CI tests
    Add ability to run producer-consumer CI tests when using an allocation
    of two or more nodes. The write example is used on the initial half of
    the nodes within an allocation as the producer, and the read example
    is used on the latter half of the nodes with an allocation as the
    consumer. Currently not set up to account for an odd number of nodes
    within an allocation.
    
    Suite runs the same series of tests as the other suites; using varying
    sizes, shared file or file-per-process, and the static or gotcha
    linked executables. It defaults to using POSIX-IO. Running with MPI-IO
    is also available, however they are not being run in RUN_CI_TESTS.sh
    as there currently are bugs when running the write/read examples with
    MPI-IO.
    
    Each test's parameters are passed to `unify_run_test` using producer or
    consumer in the app name. This is to prevent file name conflicts
    within UnifyFS, or in error and output file names. The
    `build_test_command` function has been adjusted to parse this name out
    and in turn run the appropriate example executable.
    Within these functions other adjustments have been made to account for
    the producer-consumer workflow. These include using the correct
    filename when consuming and using a JOB_RUN_COMMAND that excludes half
    of the available hosts.
    As future workflows are added, may want to refactor these functions
    into separate workflow functions.
    
    Functions to get the latter and initial hosts as a comma separated
    list as well as variables needed for running on half the allocated
    nodes are set up in the respective resource manager setup scripts.

--- t/ci/RUN_CI_TESTS.sh
+++ t/ci/RUN_CI_TESTS.sh
@@ -121,6 +121,14 @@ source $UNIFYFS_CI_DIR/100-writeread-tests.sh --mpiio
 # MPI-IO read example tests
 #source $UNIFYFS_CI_DIR/120-read-tests.sh --mpiio
 
+### Producer-Consumer workload tests
+
+# POSIX-IO producer-consumer tests
+source $UNIFYFS_CI_DIR/300-producer-consumer-tests.sh
+
+# MPI-IO producer-consumer tests
+#source $UNIFYFS_CI_DIR/300-producer-consumer-tests.sh --mpiio
+
 ##############################################################################
 # DO NOT add additional tests after this point
 ##############################################################################
--- t/ci/setup-lsf.sh
+++ t/ci/setup-lsf.sh
@@ -6,26 +6,24 @@
 # Functions specific to LSF
 
 # Get the compute hosts being used in the current allocation.
-#
 # Returns the unique hosts being used on LSF, exluding the launch host.
 get_lsf_hosts()
 {
     # NOTE: It's possible that some systems with LSF may place the user on the
     # first compute node rather than a launch node.
     local l_hosts=$(uniq $LSB_DJOB_HOSTFILE | tail -n +2)
-    echo $l_hosts
+    echo "$l_hosts"
 }
 
 # Gets the compute hosts being used in the current allocation and returns them
 # as a list so they are usable by commands like pdsh.
-#
 # Returns the unique hosts being used on LSF as a list.
 # (e.g., host1,host2,...,hostn)
 get_hostlist()
 {
     local l_hosts=$(get_lsf_hosts)
-    #replace spaces with commas
-    local l_hostlist=${l_hosts//[[:blank:]]/,}
+    # Replace spaces with commas
+    local l_hostlist=$(echo $l_hosts | tr ' ' ',')
     echo $l_hostlist
 }
 
@@ -34,7 +32,7 @@ nnodes=$(get_lsf_hosts | wc -w)
 
 # Define each resource set
 nprocs=${UNIFYFS_CI_NPROCS:-1}
-ncores=${UNIFYFS_CI_NCORES:-20}
+ncores=${UNIFYFS_CI_NCORES:-18}
 
 # Total resource sets and how many per host
 nrs_per_node=${UNIFYFS_CI_NRS_PER_NODE:-1}
@@ -68,13 +66,42 @@ if [ $(($nres_sets / $nrs_per_node)) -gt $nnodes ]; then
     exit 1
 fi
 
-jsargs="-a${nprocs} -c${ncores} -r${nrs_per_node} -n${nres_sets}"
+jsargs="-a ${nprocs} -c ${ncores} -r ${nrs_per_node}"
 
 app_out="-o"
 app_err="-k"
-JOB_RUN_COMMAND="jsrun $jsargs"
+JOB_RUN_COMMAND="jsrun $jsargs -n ${nres_sets}"
 JOB_RUN_ONCE_PER_NODE="jsrun -r1"
 JOB_ID=${JOB_ID:-$LSB_JOBID}
+
+# Set up producer-consumer variables and functions when using two or more hosts
+if [[ $nnodes -ge 2 ]]; then
+    # Prereq for running 300-producer-consumer-tests.sh
+    test_set_prereq MULTIPLE_HOSTS
+
+    # Get the initial half of the hosts in the current allocation
+    get_initial_hosts()
+    {
+        local l_hosts=$(get_lsf_hosts)
+        local l_initial_hosts=$(echo "$l_hosts" | head -n $(($nnodes / 2 )))
+        # Replace spaces with commas
+        local l_initial_hostlist=$(echo $l_initial_hosts | tr ' ' ',')
+        echo $l_initial_hostlist
+    }
+
+    # Get the latter half of the hosts in the current allocation
+    get_latter_hosts()
+    {
+        local l_hosts=$(get_lsf_hosts)
+        local l_latter_hosts=$(echo "$l_hosts" | tail -n $(($nnodes / 2 )))
+        # Replace spaces with commas
+        local l_latter_hostlist=$(echo $l_latter_hosts | tr ' ' ',')
+        echo $l_latter_hostlist
+    }
+
+    exclude_option="-x"
+    JOB_RUN_HALF_NODES="jsrun $jsargs -n $(($nres_sets/2))"
+fi
 
 echo "$infomsg ====================== LSF Job Info ======================"
 echo "$infomsg ----------------------- Job Status -----------------------"
